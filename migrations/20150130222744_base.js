16e2068dce709488fb515648fd470acab06caa8e
Connor Peet <connor@peet.io>
2015-01-31 15:42:13 -0500

    Signing flow works :sparkle:

--- migrations/20150130222744_base.js
+++ migrations/20150130222744_base.js
@@ -1,20 +1,32 @@
 exports.up = function(knex, Promise) {
     return Promise.all([
-        knex.schema.createTable('users', function (table) {
+        knex.schema.createTable('user', function (table) {
             table.increments();
             table.string('username');
             table.string('email');
             table.string('password');
             table.string('device');
             table.binary('private_key');
-            table.timestamps();
         }),
 
-        knex.schema.createTable('clients', function (table) {
+        knex.schema.createTable('client', function (table) {
             table.string('id').primary();
             table.string('secret');
             table.string('name');
-            table.timestamps();
+        }),
+
+        knex.schema.createTable('external_client', function (table) {
+            table.string('id').primary();
+            table.string('secret');
+            table.string('name');
+        }),
+
+        knex.schema.createTable('external_access_token', function (table) {
+            table.string('id').primary();
+            table.dateTime('expiration');
+            table.string('token').index();
+            table.integer('user').index();
+            table.dateTime('created_at');
         }),
 
         knex.schema.createTable('access_token', function (table) {
@@ -22,7 +34,6 @@ exports.up = function(knex, Promise) {
             table.dateTime('expiration');
             table.string('token').index();
             table.integer('user').index();
-            table.timestamps();
         }),
     ]);
 };
